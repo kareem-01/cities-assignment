6349bf1f62542efb7678d71d9aebb2cd6d4b75d1
kareem <kareemesam52@yahoo.com>
2024-08-07 00:55:48 +0300

    implemented binarySearch

--- app/src/main/java/com/example/citiesassignment/utils.kt
+++ app/src/main/java/com/example/citiesassignment/utils.kt
@@ -1,5 +1,8 @@
 package com.example.citiesassignment
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.ui.Modifier
 import com.example.citiesassignment.data.models.City
 
 fun List<City>.mergeSort(cities: MutableList<City> = this.toMutableList()): MutableList<City> {
@@ -13,7 +16,7 @@ fun List<City>.mergeSort(cities: MutableList<City> = this.toMutableList()): Muta
     return merge(mergeSort(left), mergeSort(right))
 }
 
-fun merge(left: MutableList<City>, right: MutableList<City>): MutableList<City> {
+private fun merge(left: MutableList<City>, right: MutableList<City>): MutableList<City> {
     var indexLeft = 0
     var indexRight = 0
     val newList: MutableList<City> = mutableListOf()
@@ -39,4 +42,55 @@ fun merge(left: MutableList<City>, right: MutableList<City>): MutableList<City> 
     }
 
     return newList
+}
+
+fun Modifier.noRippleClick(onClick: () -> Unit): Modifier {
+    val interaction = MutableInteractionSource()
+    return this.then(
+        Modifier.clickable(
+            interactionSource = interaction,
+            indication = null,
+            onClick = onClick
+        )
+    )
+}
+
+fun List<City>.cityBinarySearch( target: String): List<City>? {
+    val cities: List<City> = this
+    if (cities.isEmpty()) return null
+
+    var low = 0
+    var high = cities.size - 1
+    val result = mutableListOf<City>()
+
+    while (low <= high) {
+        val mid = (low + high) / 2
+        val midCity = cities[mid]
+
+        when {
+            midCity.name.startsWith(target, ignoreCase = false) -> {
+                // Add the midCity to the result list
+                result.add(midCity)
+
+                // Search for other cities starting with the target in the left half
+                var leftIndex = mid - 1
+                while (leftIndex >= 0 && cities[leftIndex].name.startsWith(target, ignoreCase = true)) {
+                    result.add(cities[leftIndex])
+                    leftIndex--
+                }
+
+                // Search for other cities starting with the target in the right half
+                var rightIndex = mid + 1
+                while (rightIndex < cities.size && cities[rightIndex].name.startsWith(target, ignoreCase = true)) {
+                    result.add(cities[rightIndex])
+                    rightIndex++
+                }
+
+                return result
+            }
+            midCity.name.compareTo(target, ignoreCase = true) < 0 -> low = mid + 1
+            else -> high = mid - 1
+        }
+    }
+    return null
 }
